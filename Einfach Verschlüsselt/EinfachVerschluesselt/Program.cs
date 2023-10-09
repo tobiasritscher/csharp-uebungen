dac2d254090fca3e9246aab7558211cd81b64869
Tobias Ritscher <97285266+tobiasritscher@users.noreply.github.com>
2023-10-09 05:33:46 +0200

    Add files via upload

---
+++ Einfach Verschlüsselt/EinfachVerschluesselt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EinfachVerschluesselt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text (unsicher) verschlüsseln mit ROT-13!");
+            Console.WriteLine();
+
+            // Deine Lösung hier !!!
+
+            Console.WriteLine("Zum Beenden bitte 'Enter' drücken");
+            Console.ReadLine();
+        }
+    }
+}
